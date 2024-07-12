8698233e2f2a5f3f352f8e489c3491dbef815e65
Felipe Brum <fbrump@gmail.com>
2024-07-12 23:06:24 +0100

    feat: Create articles page structure
    - Add some structures on articles page
    - Get some articles from real world
    
    ticket: #5

--- system-layouts/src/components/ArticlesPage.vue
+++ system-layouts/src/components/ArticlesPage.vue
@@ -6,6 +6,188 @@
   <header>
     <h1>Articles</h1>
   </header>
+  <section>
+    <article>
+      <header>
+        <h2>
+          Python in Daily Work
+        </h2>
+      </header>
+      <section>
+        <p>
+          Python is not just for professional programmers. Learning Python can dramatically change the daily work of marketers, researchers, doctors, and even artists. Let’s see how Python is enhancing jobs in different industries.
+        </p>
+      </section>
+      <footer>
+        <address>
+          By Marky
+        </address>
+        <router-link to="/articles/python-in-daily-work">
+          Read More
+        </router-link>
+      </footer>
+    </article>
+    <article>
+      <header>
+        <h2>
+          How to Organize your Life
+        </h2>
+      </header>
+      <section>
+        <p>
+          In a world bursting at the seams with tasks and distractions, have you ever come across individuals who seem perpetually disheveled? Those who perpetually miss deadlines or misplace items, and as a result, wear their stress like a heavy cloak?
+        </p>
+      </section>
+      <footer>
+        <address>
+          By Leon Ho
+        </address>
+        <router-link to="/articles/how-to-organize-your-life">
+          Read More
+        </router-link>
+      </footer>
+    </article>
+    <article>
+      <header>
+        <h2>
+          Linux and Open Source
+        </h2>
+      </header>
+      <section>
+        <p>
+          When Linus Torvalds introduced Linux to the world in the early ’90s, he might not have imagined the colossal footprint it would leave on the tech world. Fast forward to today, Linux isn’t just an enthusiast’s project — it’s the silent engine powering global tech giants, devices in our pockets, and the infrastructure of the internet itself.
+        </p>
+      </section>
+      <footer>
+        <address>
+          By April Bohnert
+        </address>
+        <router-link to="/articles/linux-and-open-source">
+          Read More
+        </router-link>
+      </footer>
+    </article>
+    <article>
+      <header>
+        <h2>
+          Monolithic Architecture
+        </h2>
+      </header>
+      <section>
+        <p>
+          A monolithic architecture is the traditional unified model for the design of a software program. Monolithic, in this context, means "composed all in one piece." According to the Cambridge dictionary, the adjective monolithic also means both "too large" and "unable to be changed."
+        </p>
+      </section>
+      <footer>
+        <address>
+          By Rahul Awati & Ivy Wigmore
+        </address>
+        <router-link to="/articles/monolithic-architecture">
+          Read More
+        </router-link>
+      </footer>
+    </article>
+    <article>
+      <header>
+        <h2>
+          How To Take Study Notes
+        </h2>
+      </header>
+      <section>
+        <p>
+          If your in-class notes are messy, unorganized, and unclear at first glance, you’re not going to get much use out of them. This has nothing to do with how neat your handwriting is — it’s all about how your notes are structured.
+        </p>
+      </section>
+      <footer>
+        <address>
+          By J.K. Hiu
+        </address>
+        <router-link to="/articles/how-to-take-study-notes">
+          Read More
+        </router-link>
+      </footer>
+    </article>
+    <article>
+      <header>
+        <h2>
+          The impact of AI on Study
+        </h2>
+      </header>
+      <section>
+        <p>
+          AI-based tools such as ChatGPT and GPT-4 are currently changing the university landscape and in many places, the consequences for future forms of teaching and examination are already being discussed. In order to create an empirical basis for this, a nationwide survey of students was carried out in order to analyse the use and possible characteristics of AI-based tools that are important to students. 
+        </p>
+      </section>
+      <footer>
+        <address>
+          By Jörg von Garrel
+        </address>
+        <router-link to="/articles/the-impact-of-ai-on-study">
+          Read More
+        </router-link>
+      </footer>
+    </article>
+    <article>
+      <header>
+        <h2>
+          What Is Minimalism?
+        </h2>
+      </header>
+      <section>
+        <p>
+          So what is this minimalism thing? It’s quite simple: to be a minimalist you must live with less than 100 things, you can’t own a car or a home or a television, you can’t have a career, you must live in exotic hard-to-pronounce places all over the world, you must start a blog, you can’t have children, and you must be a young white male from a privileged background.
+        </p>
+      </section>
+      <footer>
+        <address>
+          By Bluehost
+        </address>
+        <router-link to="/articles/what-is-minimalism">
+          Read More
+        </router-link>
+      </footer>
+    </article>
+    <article>
+      <header>
+        <h2>
+          Work-Life Balance
+        </h2>
+      </header>
+      <section>
+        <p>
+          Boundaries between work and life have been dissolving as cell phones and the internet made it possible to reach people outside the office.
+        </p>
+      </section>
+      <footer>
+        <address>
+          By Yunuen Bonaparte
+        </address>
+        <router-link to="/articles/work-life-balance">
+          Read More
+        </router-link>
+      </footer>
+    </article>
+    <article>
+      <header>
+        <h2>
+          Motivate Your Kids
+        </h2>
+      </header>
+      <section>
+        <p>
+          Why is it so hard to motivate kids? As parents, we often have a funny, inaccurate belief that our children won’t care unless we twist their arms. But the simple truth is that your attempts to motivate your child are probably working against you.
+        </p>
+      </section>
+      <footer>
+        <address>
+          By Debbie Pincus
+        </address>
+        <router-link to="/articles/motivate-your-kids">
+          Read More
+        </router-link>
+      </footer>
+    </article>
+  </section>
 </template>
 
 <style scoped>
