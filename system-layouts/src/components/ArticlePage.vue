f751ce505bb0fc522b49451ba9f7164eeceb8a4b
Felipe Brum <fbrump@gmail.com>
2024-07-20 22:02:13 +0100

    feat: Add article HTML structure
    - Add fake image for the article
    - Add html structure with article, header, and p/sections
    
    ticket: #6

--- system-layouts/src/components/ArticlePage.vue
+++ system-layouts/src/components/ArticlePage.vue
@@ -1,11 +1,49 @@
 <script setup>
 
+const props = defineProps({
+  slug: {
+    type: String,
+    required: true
+  }
+})
+
 </script>
 
 <template>
+  <article>
   <header>
-    <h1>Article</h1>
+    <h1>{{ props.slug }}</h1>
   </header>
+  <section>
+    <p>
+      If you are considering learning Python, you are one step closer to a key milestone in your work life. Coding is an important skill for people of various roles, not just programmers. To keep up with the changes, you need to develop new skills. Knowing Python can be a competitive advantage in tomorrow’s job market.
+    </p>
+    <p>
+      But there are many programming languages. Why should you choose Python? There’s a simple answer to this question. The simple answer is that Python is pretty easy to get started with.
+    </p>
+    <p>
+      First, it has a simple syntax that makes it very accessible to people without an IT background. Second, Python is highly productive and usually requires fewer lines of code than other programming languages. Finally, Python is versatile and can be used for just about anything, including analyzing and visualizing data, creating applications, building blockchains, and much more.
+    </p>
+  </section>
+  <section>
+    <img src="@/assets/images/article-1.png" alt="Article Image" title="Article image" width="280px">
+  </section>
+  <section>
+    <p>
+      With all these advantages, Python has made it to the top of the most popular programming languages. During the last few years, it’s become an UNDISPUTED LEADER in programming language tutorial searches on Google, leaving Java, JavaScript, C#, and others far behind. Of course, other programming languages also have their benefits. For example, Java is faster than Python. (For more differences between Python and Java, check out THIS CONCISE COMPARISON GUIDE.) You can learn more about why Python is so incredibly popular in THIS ARTICLE.
+    </p>
+    <p>
+      As mentioned before, one of the reasons behind the high popularity of Python is its applicability to a variety of tasks. There is a popular book by Al Sweigart called AUTOMATE THE BORING STUFF WITH PYTHON. It provides numerous examples of Python doing in minutes what would take hours to do by hand. The most interesting part is that you need only basic knowledge of Python to apply the solutions in this book.
+    </p>
+    <p>
+      When working on long and complex projects, you often end up with dozens of folders and hundreds or thousands of files related to that project. In the ideal world, you want these files to be organized in a specific way, with file names following certain naming conventions. In the real world, lots of parallel assignments with tight deadlines often result in total chaos inside your project folder. Luckily, there are certain Python modules that can rescue you from renaming, copying, moving, and compressing all these files by hand.
+    </p>
+    <p>
+      You find PDF files everywhere, storing reports, texts, graphs, images, etc. It’s a great way to share nicely formatted information, but unfortunately PDFs are usually very hard to work with when you do anything other than read them. Here again, Python comes to the rescue. Certain Python modules can help you manipulate PDFs, enabling you to extract text from them, combine select pages from multiple files, add a logo or watermark to the specified pages, and much more.
+      An example of such a module is OCR (Optical Character Recognition), which is used in an online PDF TO WORD converter. It allows tools to retrieve text from PDFs and images and convert them into editable text.
+    </p>
+  </section>
+  </article>
 </template>
 
 <style scoped>
