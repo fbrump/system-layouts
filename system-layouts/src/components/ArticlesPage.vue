e6f3c9bad2a867522cf2f548a1dad9d96aac283e
Felipe Brum <fbrump@gmail.com>
2024-09-02 22:57:47 +0100

    feat: Initial work on Articles page
    - Add card for each article
    - Add some colors for article header, content and footer
    
    ticket: #9

--- system-layouts/src/components/ArticlesPage.vue
+++ system-layouts/src/components/ArticlesPage.vue
@@ -3,186 +3,188 @@
 </script>
 
 <template>
-  <header>
-    <h1>Articles</h1>
+  <header class="container mx-auto">
+    <h1 class="text-center text-3xl font-bold py-7">
+      Articles
+    </h1>
   </header>
-  <section>
-    <article>
-      <header>
-        <h2>
+  <section class="grid grid-cols-3 gap-4 justify-center">
+    <article class="hover:font-mono">
+      <header class="bg-green-700 rounded-md">
+        <h2 class="text-xl text-green-950 font-semibold p-4">
           Python in Daily Work
         </h2>
       </header>
-      <section>
-        <p>
+      <section class="bg-amber-100 px-2 py-1 min-h-80">
+        <p class="indent-4">
           Python is not just for professional programmers. Learning Python can dramatically change the daily work of marketers, researchers, doctors, and even artists. Let’s see how Python is enhancing jobs in different industries.
         </p>
       </section>
-      <footer>
+      <footer class="bg-amber-500 p-2 text-center">
         <address>
           By Marky
         </address>
-        <router-link to="/articles/python-in-daily-work">
+        <router-link to="/articles/python-in-daily-work" class="text-center bg-amber-700 rounded-full p-1">
           Read More
         </router-link>
       </footer>
     </article>
-    <article>
-      <header>
-        <h2>
+    <article class="hover:font-mono">
+      <header class="bg-green-700 rounded-md">
+        <h2 class="text-xl text-green-950 font-semibold p-4">
           How to Organize your Life
         </h2>
       </header>
-      <section>
-        <p>
+      <section class="bg-amber-100 px-2 py-1 min-h-80">
+        <p class="indent-4">
           In a world bursting at the seams with tasks and distractions, have you ever come across individuals who seem perpetually disheveled? Those who perpetually miss deadlines or misplace items, and as a result, wear their stress like a heavy cloak?
         </p>
       </section>
-      <footer>
+      <footer class="bg-amber-500 p-2 text-center">
         <address>
           By Leon Ho
         </address>
-        <router-link to="/articles/how-to-organize-your-life">
+        <router-link to="/articles/how-to-organize-your-life" class="text-center bg-amber-700 rounded-full p-1">
           Read More
         </router-link>
       </footer>
     </article>
-    <article>
-      <header>
-        <h2>
+    <article class="hover:font-mono">
+      <header class="bg-green-700 rounded-md">
+        <h2 class="text-xl text-green-950 font-semibold p-4">
           Linux and Open Source
         </h2>
       </header>
-      <section>
-        <p>
+      <section class="bg-amber-100 px-2 py-1 min-h-80">
+        <p class="indent-4">
           When Linus Torvalds introduced Linux to the world in the early ’90s, he might not have imagined the colossal footprint it would leave on the tech world. Fast forward to today, Linux isn’t just an enthusiast’s project — it’s the silent engine powering global tech giants, devices in our pockets, and the infrastructure of the internet itself.
         </p>
       </section>
-      <footer>
+      <footer class="bg-amber-500 p-2 text-center">
         <address>
           By April Bohnert
         </address>
-        <router-link to="/articles/linux-and-open-source">
+        <router-link to="/articles/linux-and-open-source" class="text-center bg-amber-700 rounded-full p-1">
           Read More
         </router-link>
       </footer>
     </article>
-    <article>
-      <header>
-        <h2>
+    <article class="hover:font-mono">
+      <header class="bg-green-700 rounded-md">
+        <h2 class="text-xl text-green-950 font-semibold p-4">
           Monolithic Architecture
         </h2>
       </header>
-      <section>
-        <p>
+      <section class="bg-amber-100 px-2 py-1 min-h-80">
+        <p class="indent-4">
           A monolithic architecture is the traditional unified model for the design of a software program. Monolithic, in this context, means "composed all in one piece." According to the Cambridge dictionary, the adjective monolithic also means both "too large" and "unable to be changed."
         </p>
       </section>
-      <footer>
+      <footer class="bg-amber-500 p-2 text-center">
         <address>
           By Rahul Awati & Ivy Wigmore
         </address>
-        <router-link to="/articles/monolithic-architecture">
+        <router-link to="/articles/monolithic-architecture" class="text-center bg-amber-700 rounded-full p-1">
           Read More
         </router-link>
       </footer>
     </article>
-    <article>
-      <header>
-        <h2>
+    <article class="hover:font-mono">
+      <header class="bg-green-700 rounded-md">
+        <h2 class="text-xl text-green-950 font-semibold p-4">
           How To Take Study Notes
         </h2>
       </header>
-      <section>
-        <p>
+      <section class="bg-amber-100 px-2 py-1 min-h-80">
+        <p class="indent-4">
           If your in-class notes are messy, unorganized, and unclear at first glance, you’re not going to get much use out of them. This has nothing to do with how neat your handwriting is — it’s all about how your notes are structured.
         </p>
       </section>
-      <footer>
+      <footer class="bg-amber-500 p-2 text-center">
         <address>
           By J.K. Hiu
         </address>
-        <router-link to="/articles/how-to-take-study-notes">
+        <router-link to="/articles/how-to-take-study-notes" class="text-center bg-amber-700 rounded-full p-1">
           Read More
         </router-link>
       </footer>
     </article>
-    <article>
-      <header>
-        <h2>
+    <article class="hover:font-mono">
+      <header class="bg-green-700 rounded-md">
+        <h2 class="text-xl text-green-950 font-semibold p-4">
           The impact of AI on Study
         </h2>
       </header>
-      <section>
-        <p>
+      <section class="bg-amber-100 px-2 py-1 min-h-80">
+        <p class="indent-4">
           AI-based tools such as ChatGPT and GPT-4 are currently changing the university landscape and in many places, the consequences for future forms of teaching and examination are already being discussed. In order to create an empirical basis for this, a nationwide survey of students was carried out in order to analyse the use and possible characteristics of AI-based tools that are important to students. 
         </p>
       </section>
-      <footer>
+      <footer class="bg-amber-500 p-2 text-center">
         <address>
           By Jörg von Garrel
         </address>
-        <router-link to="/articles/the-impact-of-ai-on-study">
+        <router-link to="/articles/the-impact-of-ai-on-study" class="text-center bg-amber-700 rounded-full p-1">
           Read More
         </router-link>
       </footer>
     </article>
-    <article>
-      <header>
-        <h2>
+    <article class="hover:font-mono">
+      <header class="bg-green-700 rounded-md">
+        <h2 class="text-xl text-green-950 font-semibold p-4">
           What Is Minimalism?
         </h2>
       </header>
-      <section>
-        <p>
+      <section class="bg-amber-100 px-2 py-1 min-h-80">
+        <p class="indent-4">
           So what is this minimalism thing? It’s quite simple: to be a minimalist you must live with less than 100 things, you can’t own a car or a home or a television, you can’t have a career, you must live in exotic hard-to-pronounce places all over the world, you must start a blog, you can’t have children, and you must be a young white male from a privileged background.
         </p>
       </section>
-      <footer>
+      <footer class="bg-amber-500 p-2 text-center">
         <address>
           By Bluehost
         </address>
-        <router-link to="/articles/what-is-minimalism">
+        <router-link to="/articles/what-is-minimalism" class="text-center bg-amber-700 rounded-full p-1">
           Read More
         </router-link>
       </footer>
     </article>
-    <article>
-      <header>
-        <h2>
+    <article class="hover:font-mono">
+      <header class="bg-green-700 rounded-md">
+        <h2 class="text-xl text-green-950 font-semibold p-4">
           Work-Life Balance
         </h2>
       </header>
-      <section>
-        <p>
+      <section class="bg-amber-100 px-2 py-1 min-h-80">
+        <p class="indent-4">
           Boundaries between work and life have been dissolving as cell phones and the internet made it possible to reach people outside the office.
         </p>
       </section>
-      <footer>
+      <footer class="bg-amber-500 p-2 text-center">
         <address>
           By Yunuen Bonaparte
         </address>
-        <router-link to="/articles/work-life-balance">
+        <router-link to="/articles/work-life-balance" class="text-center bg-amber-700 rounded-full p-1">
           Read More
         </router-link>
       </footer>
     </article>
-    <article>
-      <header>
-        <h2>
+    <article class="hover:font-mono">
+      <header class="bg-green-700 rounded-md">
+        <h2 class="text-xl text-green-950 font-semibold p-4">
           Motivate Your Kids
         </h2>
       </header>
-      <section>
-        <p>
+      <section class="bg-amber-100 px-2 py-1 min-h-80">
+        <p class="indent-4">
           Why is it so hard to motivate kids? As parents, we often have a funny, inaccurate belief that our children won’t care unless we twist their arms. But the simple truth is that your attempts to motivate your child are probably working against you.
         </p>
       </section>
-      <footer>
+      <footer class="bg-amber-500 p-2 text-center">
         <address>
           By Debbie Pincus
         </address>
-        <router-link to="/articles/motivate-your-kids">
+        <router-link to="/articles/motivate-your-kids" class="text-center bg-amber-700 rounded-full p-1">
           Read More
         </router-link>
       </footer>
