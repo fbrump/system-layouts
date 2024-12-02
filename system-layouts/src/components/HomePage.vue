91662eaa9622c33ea5f1c74a12342efa3a09db80
Felipe Brum <fbrump@gmail.com>
2024-12-02 22:53:20 +0000

    feat: Apply CSS on Home Page
    - Add sections
    - Define layout
    - Update details of each service
    - Add other imporvements
    
    ticket: #8

--- system-layouts/src/components/HomePage.vue
+++ system-layouts/src/components/HomePage.vue
@@ -3,12 +3,14 @@
 </script>
 
 <template>
-  <header>
-    <h1>Home Page</h1>
+  <header class="basis-full mx-auto bg-amber-100">
+    <h1 class="text-center text-3xl text-green-800 font-bold py-7">
+      Home Page
+    </h1>
   </header>
-  <section>
-    <header>
-      <h2>
+  <section class="px-5 py-16 text-center">
+    <header class="basis-full mx-auto">
+      <h2 class="text-2xl font-bold py-7">
         The Power of Visual Thinking in Blog Article Writing
       </h2>
     </header>
@@ -16,9 +18,9 @@
       Do you ever feel stuck when trying to come up with blog article ideas? Visual thinking might just be the solution you need to get those creative juices flowing! In this article, we'll explore how incorporating visual elements into your writing can enhance both the quality and effectiveness of your blog articles.
     </p>
   </section>
-  <section>
-    <header>
-      <h2>
+  <section class="px-5 py-16 bg-gray-900 text-white text-center">
+    <header class="basis-full mx-auto">
+      <h2 class="text-2xl font-bold py-7">
         Promoting Your Product Development Designer Services
       </h2>
     </header>
@@ -26,81 +28,134 @@
       In conclusion, promoting your product development designer services through blog articles can significantly help you attract more clients and grow your business. By following the tips mentioned above and consistently creating high-quality content, you can establish yourself as a reputable development designer in the industry and stand out from the competition.
     </p>
   </section>
-  <section>
-    <header>
-      <h2>Services</h2>
+  <section class="px-5 py-16">
+    <header class="basis-full mx-auto text-center">
+      <h2 class="text-2xl font-bold py-7">
+        Services
+      </h2>
     </header>
-    <details open>
-      <summary>Service One</summary>
-      <header>
-        <h3>
-          TOP 1
-        </h3>
-      </header>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia dicta aut ad reprehenderit libero. Omnis doloribus labore blanditiis, quidem, rerum ab esse a provident, accusantium nesciunt exercitationem quis cupiditate nemo.
-      </p>
-    </details>
-    <details open>
-      <header>
-        <h3>
-          TOP 2
-        </h3>
-      </header>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia dicta aut ad reprehenderit libero. Omnis doloribus labore blanditiis, quidem, rerum ab esse a provident, accusantium nesciunt exercitationem quis cupiditate nemo.
-      </p>
-    </details>
-    <details open>
-      <header>
-        <h3>
-          TOP 3
-        </h3>
-      </header>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia dicta aut ad reprehenderit libero. Omnis doloribus labore blanditiis, quidem, rerum ab esse a provident, accusantium nesciunt exercitationem quis cupiditate nemo.
-      </p>
-    </details>
+    <div class="container mx-auto">
+      <details open class="border-2 border-dashed border-stone-500 p-4 [&_svg]:open:-rotate-180">
+        <summary class="flex cursor-pointer list-none items-center gap-4">
+          <div>
+            <svg class="rotate-0 transform text-blue-700 transition-all duration-300" fill="none" height="20" width="20" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+              <polyline points="6 9 12 15 18 9"></polyline>
+            </svg>
+          </div>
+          <div>Service One</div>
+        </summary>
+        <header>
+          <h3>
+            TOP 1
+          </h3>
+        </header>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia dicta aut ad reprehenderit libero. Omnis doloribus labore blanditiis, quidem, rerum ab esse a provident, accusantium nesciunt exercitationem quis cupiditate nemo.
+        </p>
+      </details>
+    </div>
+    <div class="container mx-auto">
+      <details open class="border-2 border-dashed border-stone-500 p-4 [&_svg]:open:-rotate-180">
+        <summary class="flex cursor-pointer list-none items-center gap-4">
+          <div>
+            <svg class="rotate-0 transform text-blue-700 transition-all duration-300" fill="none" height="20" width="20" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+              <polyline points="6 9 12 15 18 9"></polyline>
+            </svg>
+          </div>
+          <div>Service Two</div>
+        </summary>
+        <header>
+          <h3>
+            TOP 2
+          </h3>
+        </header>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia dicta aut ad reprehenderit libero. Omnis doloribus labore blanditiis, quidem, rerum ab esse a provident, accusantium nesciunt exercitationem quis cupiditate nemo.
+        </p>
+      </details>
+    </div>
+    <div class="container mx-auto">
+      <details open class="border-2 border-dashed border-stone-500 p-4 [&_svg]:open:-rotate-180">
+        <summary class="flex cursor-pointer list-none items-center gap-4">
+          <div>
+            <svg class="rotate-0 transform text-blue-700 transition-all duration-300" fill="none" height="20" width="20" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+              <polyline points="6 9 12 15 18 9"></polyline>
+            </svg>
+          </div>
+          <div>Service Three</div>
+        </summary>
+        <header>
+          <h3>
+            TOP 3
+          </h3>
+        </header>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quia dicta aut ad reprehenderit libero. Omnis doloribus labore blanditiis, quidem, rerum ab esse a provident, accusantium nesciunt exercitationem quis cupiditate nemo.
+        </p>
+      </details>
+    </div>
   </section>
-  <section>
-    <header>
-      <h2>
+  <section class="px-5 py-16 bg-green-950 text-amber-200">
+    <header class="basis-full mx-auto text-center">
+      <h2 class="text-2xl font-bold py-7">
         Testimonials
       </h2>
       <em>Check what people has been talking about us.</em>
     </header>
-    <figure>
-      <img src="https://petapixel.com/assets/uploads/2020/02/ERtaYWRWAAAKH-k-800x800.jpg" alt="Testimonial - Maria">
-      <figurecaption>
-        Maria - "We don’t need to spend as much time in meetings now that we use WebSite."
-      </figurecaption>
-    </figure>
-    <figure>
-      <img src="https://petapixel.com/assets/uploads/2020/02/ERtZcPpWsAESTfl.jpg" alt="Testimonial - John">
-      <figurecaption>
-        John - "WebSite is a game-changer. Instead of drowning in an endless chain of emails, there is clear and easy accountability meaning tasks actually get done!"
-      </figurecaption>
-    </figure>
-    <figure>
-      <img src="https://petapixel.com/assets/uploads/2020/02/ERtZcPrXkAAmOhG-800x800.jpg" alt="Testimonial - Patr">
-      <figurecaption>
-        Patrick - "If you are a business owner, and you don't have WebSite in your toolkit just yet, I highly recommend that you check it out."
-      </figurecaption>
-    </figure>
-    <figure>
-      <img src="https://asamnews.com/wp-content/uploads/2019/02/thispersondoesnotexist.com_.jpg" alt="Testimonial One">
-      <figurecaption>
-        Rebecca - "I have tried a few software of this kind and WebSite is the best by far!"
-      </figurecaption>
-    </figure>
+    <div class="grid gap-4 grid-cols-2 xl:grid-cols-4">
+      <figure class="p-2 shadow-md bg-green-900 hover:bg-green-800">
+        <img src="https://petapixel.com/assets/uploads/2020/02/ERtaYWRWAAAKH-k-800x800.jpg" alt="Testimonial - Maria">
+        <figurecaption>
+          <span class="overline uppercase font-mono">
+            Maria
+          </span>
+          <blockquote class="hover:text-lg italic font-semibold text-gray-900 dark:text-white">
+            "We don’t need to spend as much time in meetings now that we use WebSite."
+          </blockquote>
+        </figurecaption>
+      </figure>
+      <figure class="p-2 shadow-md bg-green-900 hover:bg-green-800">
+        <img src="https://petapixel.com/assets/uploads/2020/02/ERtZcPpWsAESTfl.jpg" alt="Testimonial - John">
+        <figurecaption>
+          <span class="overline uppercase font-mono">
+            John
+          </span>
+          <blockquote class="hover:text-lg italic font-semibold text-gray-900 dark:text-white">
+            "WebSite is a game-changer. Instead of drowning in an endless chain of emails, there is clear and easy accountability meaning tasks actually get done!"
+          </blockquote>
+        </figurecaption>
+      </figure>
+      <figure class="p-2 shadow-md bg-green-900 hover:bg-green-800">
+        <img src="https://petapixel.com/assets/uploads/2020/02/ERtZcPrXkAAmOhG-800x800.jpg" alt="Testimonial - Patr">
+        <figurecaption>
+          <span class="overline uppercase font-mono">
+            Patrick
+          </span>
+          <blockquote class="hover:text-lg italic font-semibold text-gray-900 dark:text-white">
+            "If you are a business owner, and you don't have WebSite in your toolkit just yet, I highly recommend that you check it out."
+          </blockquote>
+        </figurecaption>
+      </figure>
+      <figure class="p-2 shadow-md bg-green-900 hover:bg-green-800">
+        <img src="https://asamnews.com/wp-content/uploads/2019/02/thispersondoesnotexist.com_.jpg" alt="Testimonial One">
+        <figurecaption>
+          <span class="overline uppercase font-mono">
+            Rebecca
+          </span>
+          <blockquote class="hover:text-lg italic font-semibold text-gray-900 dark:text-white">
+            "I have tried a few software of this kind and WebSite is the best by far!"
+          </blockquote>
+        </figurecaption>
+      </figure>
+    </div>
   </section>
-  <section>
-    <header>
-      <h2>
+  <section class="px-5 py-16 text-center">
+    <header class="basis-full mx-auto">
+      <h2 class="text-2xl font-bold py-7">
         About us
       </h2>
     </header>
-    <p>With about us, we have finally accomplished things that have been waiting forever to get done.</p>
+    <p class="py-10">With about us, we have finally accomplished things that have been waiting forever to get done.</p>
   </section>
 </template>
 
